d75ecad4d8a64fe13244ee95105ff41e82825c81
Sandesh Sharma <sansha98@gmail.com>
2020-10-15 09:30:26 +0200

    last commit with whole window

--- ps2_python_template/ps2.py
+++ ps2_python_template/ps2.py
@@ -11,9 +11,9 @@ R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
 # Compute disparity (using method disparity_ssd defined in disparity_ssd.py)
 from disparity_ssd import disparity_ssd
 
-first_exercise = False
+first_exercise = True
 second_exercise = False
-third_exercise = True
+third_exercise = False
 if first_exercise:
     L = cv2.imread(os.path.join('input', 'pair0-L.png'), 0) * (1.0 / 255.0)  # grayscale, [0, 1]
     R = cv2.imread(os.path.join('input', 'pair0-R.png'), 0) * (1.0 / 255.0)
@@ -23,11 +23,13 @@ if first_exercise:
 
     # TODO: Save output images (D_L as output/ps2-1-a-1.png and D_R as output/ps2-1-a-2.png)
     # Note: They may need to be scaled/shifted before saving to show results properly
-    D_L_scaled = D_L * (255.0 / (np.max(D_L) - np.min(D_L)) + np.min(D_L)).astype('uint8')
-    D_R_scaled = D_R * (255.0 / (np.max(D_R) - np.min(D_R)) + np.min(D_R)).astype('uint8')
+    # D_L_scaled = D_L * (255.0 / (np.max(D_L) - np.min(D_L)) + np.min(D_L)).astype('uint8')
+    # D_R_scaled = D_R * (255.0 / (np.max(D_R) - np.min(D_R)) + np.min(D_R)).astype('uint8')
+    D_L_scaled = ((D_L - np.min(D_L)) / (np.max(D_L) - np.min(D_L)) * 255.0).astype('uint8')
+    D_R_scaled = ((D_R - np.min(D_R)) / (np.max(D_R) - np.min(D_R)) * 255.0).astype('uint8')
 
-    D_L_scaled = (D_L * (255.0 / 2.0)).astype('uint8')
-    D_R_scaled = (D_R * (255.0 / -2.0)).astype('uint8')
+    #D_L_scaled = (D_L * (255.0 / 2.0)).astype('uint8')
+    #D_R_scaled = (D_R * (255.0 / -2.0)).astype('uint8')
 
 
     cv2.imshow("", D_L_scaled)
@@ -35,8 +37,8 @@ if first_exercise:
     cv2.imshow("", D_R_scaled)
     cv2.waitKey(0)
 
-    cv2.imwrite("output/ps2-1-a-5.png", D_L_scaled)
-    cv2.imwrite("output/ps2-1-a-6.png", D_R_scaled)
+    cv2.imwrite("output/ps2-1-a-8.png", D_L_scaled)
+    cv2.imwrite("output/ps2-1-a-7.png", D_R_scaled)
 # TODO: Rest of your code here
 if second_exercise:
     L_rgb = cv2.imread(os.path.join('input', 'pair1-L.png'))   # grayscale, [0, 1]
