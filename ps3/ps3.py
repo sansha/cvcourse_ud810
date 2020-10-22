9c5b0dbd2b0dd5f6e5cdd7ba0dd9365c058dc48a
Sandesh Sharma <sansha98@gmail.com>
2020-10-22 09:47:18 +0200

    fixed error in epipolar line generation
    - fix wrong left and right edge points -> more lines are drawn
    - fix wrong calculation of epipolar lines a, transpose needs to be used

--- ps3/ps3.py
+++ ps3/ps3.py
@@ -2,18 +2,17 @@ import numpy as np
 import os
 from least_square_matrix import least_square_matrix
 from project import project, project_many
-from compute_center import  compute_center
+from compute_center import compute_center
 
 
 def compute_residuals(pts1, pts2):
     diff = pts1 - pts2
     sq_diff = np.square(diff)
     sum = np.sum(sq_diff, axis=1)
-    #print(sum.shape)
+    # print(sum.shape)
     sqrt = np.sqrt(sum)
-    #print(sqrt)
+    # print(sqrt)
     return sqrt
-
 
 
 def task1():
@@ -23,16 +22,15 @@ def task1():
     print(M)
     C = compute_center(M)
     print(C)
-    example3d = [ 1.2323, 1.4421, 0.4506]
+    example3d = [1.2323, 1.4421, 0.4506]
     example2d = project(M, example3d)
-    #print(example2d)
+    # print(example2d)
 
     projected2d = project_many(M, points3d)
     sqrt = compute_residuals(projected2d, points2d)
     np.savetxt("output/M_norm_pic_a.txt", M)
     np.savetxt("output/projected_norm_pic_a.txt", projected2d)
     np.savetxt("output/residuals_norm_pic_a.txt", sqrt)
-
 
 
 ## second task
@@ -49,23 +47,23 @@ def task2(num_points):
         M = least_square_matrix(points2d, points3d)
         unused_idx = np.delete(np.arange(0, points3d_all.shape[0]), sampled_idx)
         test_idx = np.random.choice(unused_idx, 4, replace=False)
-        #print(sampled_idx)
-        #print(test_idx)
+        # print(sampled_idx)
+        # print(test_idx)
         testpts2d = points2d_all[test_idx, :]
         testpts3d = points3d_all[test_idx, :]
         project2d = project_many(M, testpts3d)
         residuals = compute_residuals(testpts2d, project2d)
         avg_res = np.average(residuals)
         all_residuals[i] = avg_res
-        #print(avg_res)
+        # print(avg_res)
         if (avg_res < curr_avg):
-            #print("new best!")
+            # print("new best!")
             bestM = M
             curr_avg = avg_res
     return bestM, all_residuals
 
 
-#task1()
+# task1()
 
 # Ms = []
 # for num_points in [8, 12, 16]:
@@ -84,45 +82,88 @@ def task2(num_points):
 from fundamental_matrix import fundamental_matrix, reduce_rank
 import cv2
 
+
 def to_normal(epipoint):
     assert epipoint.shape == (3,)
-    return (int(epipoint[0] / epipoint[2]), int(epipoint[1] / epipoint[2]))
+    return (epipoint[0] / epipoint[2]), (epipoint[1] / epipoint[2])
+
+def to_int(normalpoint):
+    rounded = np.around(normalpoint)
+    return int(rounded[0]), int(rounded[1])
+
+def to_drawable(epipoint):
+    return to_int(to_normal(epipoint))
 
 def task3():
-    pts1 = np.loadtxt("input/pts2d-pic_a.txt")
-    pts2 = np.loadtxt("input/pts2d-pic_b.txt")
-    M = fundamental_matrix(pts1, pts2)
+    ptsA = np.loadtxt("input/pts2d-pic_a.txt")
+    ptsB = np.loadtxt("input/pts2d-pic_b.txt")
+
+    num_points = 20  # ptsA.shape[0]
+
+    M = fundamental_matrix(ptsA, ptsB)
     print(M)
     print(np.linalg.matrix_rank(M))
 
     F = reduce_rank(M)
     print(F)
     print(np.linalg.matrix_rank(F))
-    epipolar_lines_b = np.zeros((pts1.shape[0], 3)) # store epipolar lines
-    epipolar_lines_a = np.zeros((pts1.shape[0], 3)) # store epipolar lines
+    epipolar_lines_b = np.zeros((num_points, 3))  # store epipolar lines
+    epipolar_lines_a = np.zeros((num_points, 3))  # store epipolar lines
 
-    for i in range(pts1.shape[0]):
-        epipolar_lines_b[i] = F @ np.append(pts1[i], 1) # from a to b
-        epipolar_lines_a[i] = F @ np.append(pts2[i], 1) # from b to a
+    for i in range(num_points):
+        epipolar_lines_b[i] = F @ np.append(ptsA[i], 1)  # from a to b
+        epipolar_lines_a[i] = np.transpose(F) @ np.append(ptsB[i], 1)  # from b to a
     img_a = cv2.imread("input/pic_a.jpg")
+    img_b = cv2.imread("input/pic_b.jpg")
+
     print(img_a.shape)
-    P_UL = [0, 0, 1]
-    P_BL = [img_a.shape[0] - 1, 0, 1]
-    P_UR = [0, img_a.shape[1] - 1, 1]
-    P_BR = [img_a.shape[0] - 1, img_a.shape[1] - 1, 1]
+    wrong_axis = True
+    if not wrong_axis:
+        P_UL = [0, 0, 1]
+        P_BL = [img_a.shape[0] - 1, 0, 1]
+        P_UR = [1, img_a.shape[1] - 1, 1]
+        P_BR = [img_a.shape[0] - 1, img_a.shape[1] - 1, 1]
+    else:
+        P_UL = [0, 0, 1]
+        P_BL = [0, img_a.shape[0] - 1, 1]
+        P_UR = [img_a.shape[1] - 1, 0, 1]
+        P_BR = [img_a.shape[1] - 1, img_a.shape[0] - 1, 1]
+
     l_L = np.cross(P_UL, P_BL)
     l_R = np.cross(P_UR, P_BR)
     window_name = 'Image'
-    color = (0, 255, 0)
-    for i in range(pts1.shape[0]):
+    green = (0, 255, 0)
+    red = (0, 0, 255)
+    for i in range(num_points):
         pt_left = np.cross(epipolar_lines_a[i], l_L)
         pt_right = np.cross(epipolar_lines_a[i], l_R)
-        ptl = to_normal(pt_left)
-        ptr = to_normal(pt_right)
-        cv2.line(img_a, ptl, ptr, color)
+        offset = 40
+        ptl = to_drawable(pt_left)
+        ptr = to_drawable(pt_right)
+        adjusted_pt_l = (ptl[0], ptl[1] - offset)
+        adjusted_pt_r = (ptr[0], ptr[1] - offset)
+        cv2.line(img_a, ptr, ptl, green, thickness=1)
+
+        point = (int(ptsA[i][0]), int(ptsA[i][1]),)
+        cv2.circle(img_a, point, 5, red, thickness=4)
+
+    for i in range(num_points):
+        pt_left = np.cross(epipolar_lines_b[i], l_L)
+        pt_right = np.cross(epipolar_lines_b[i], l_R)
+        ptl = to_drawable(pt_left)
+        ptr = to_drawable(pt_right)
+        cv2.line(img_b, ptl, ptr, green, thickness=1)
+
+        point = (int(ptsB[i][0]), int(ptsB[i][1]))
+        cv2.circle(img_b, point, 5, red, thickness=4)
 
     cv2.imshow(window_name, img_a)
+    cv2.imwrite("output/pic_a_lines.png", img_a)
     cv2.waitKey(0)
+    cv2.imshow(window_name, img_b)
+    cv2.imwrite("output/pic_b_lines.png", img_b)
+    cv2.waitKey(0)
+
 
 task3()
 
@@ -136,6 +177,6 @@ task3()
 #     sqrtsum = np.sum(sqdiff)
 #     print(sqrtsum)
 
-#8 max / min: 0.023870663523427538, 0.0024005544074070387
-#12 max min: 0.009588077965169034, 0.0020345500539973482
+# 8 max / min: 0.023870663523427538, 0.0024005544074070387
+# 12 max min: 0.009588077965169034, 0.0020345500539973482
 # 16 max, min: 0.01775494910102892, 0.0023477473260722316
